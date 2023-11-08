8e1be6da9f77fc867ee2379e088ebd1588133aba
yuu <yuu.cc.minato@gmail.com>
2023-11-08 18:57:30 +0900

    fix cal.rb

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,11 +5,19 @@ require 'date'
 
 opt = OptionParser.new
 opt.on('-y VAL') do |v|
-  v if (1..12).cover?(v.to_i)
+  if (1970..2100).cover?(v.to_i)
+    v
+  else
+    abort "エラー: 正しく処理できない年です '#{v}'"
+  end
 end
 
 opt.on('-m VAL') do |v|
-  v if (1..31).cover?(v.to_i)
+  if (1..12).cover?(v.to_i)
+    v
+  else
+    abort "月の指定が正しくありません '#{v}'"
+  end
 end
 
 params = {}
@@ -36,28 +44,28 @@ class MyCal
   end
 
   def show
-    puts "　　　#{@month}月 #{@year}"
+    puts "#{@month}月 #{@year}".center(20)
     puts '日 月 火 水 木 金 土'
-    (1..31).each do |n|
-      d = Date.new(@year, @month, n)
+    first_day = Date.new(@year, @month, 1)
+    last_day  = Date.new(@year, @month, -1)
+    (first_day..last_day).each.with_index(1) do |d, n|
       youbi = d.wday
       youbi.times { print '   ' } if n == 1
 
       if d == Date.today
-        print "\e[30m\e[47m\e[5m#{d.day.to_s.rjust(2)}\e[0m"
+        print "\e[7m#{d.day.to_s.rjust(2)}\e[0m"
       else
         print d.day.to_s.rjust(2)
       end
 
       if youbi == 6
-        puts "\n"
+        puts
       else
         print ' '
       end
-    rescue StandardError
-      break
     end
-    puts "\n\n"
+    puts 
+    puts
   end
 end
 
