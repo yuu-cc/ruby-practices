ce6db041011d919587f42ef846fc5883f00edf52
yuu <yuu.cc.minato@gmail.com>
2023-11-09 08:21:35 +0900

    Made additional changes to bowling.rb

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,50 +13,35 @@ scores.each do |s|
   end
 end
 
-frames = []
-(1..10).each do |i|
+frames = (1..10).map do |i|
   if i < 10
-    frames << shots.shift(2)
+    shots.shift(2)
   else
     shots.each_with_index do |shot, j|
       shots.delete_at(j + 1) if shot == 10
     end
-    frames << shots
+    shots
   end
 end
 
 total_point = 0
 frames.each_with_index do |frame, i|
-  # 10フレーム目
-  if i == 9
-    # 単に足すだけ
-    total_point += frame.sum
-  # 9フレーム目（最終フレーム＝10フレーム目が特殊）
-  elsif i == 8
-    total_point += frame.sum
-    # ストライク
-    if frame[0] == 10
+  total_point += frame.sum
+
+  if frame[0] == 10
+    if i == 8
       total_point += frames[i + 1][0]
       total_point += frames[i + 1][1]
-    # スペア
-    elsif frame.sum == 10
-      total_point += frames[i + 1][0]
-    end
-  # それ以外のフレーム
-  else
-    total_point += frame.sum
-    # ストライク
-    if frame[0] == 10
+    elsif i < 8
       total_point += frames[i + 1][0]
       total_point += if frames[i + 1][0] == 10
                        frames[i + 2][0]
                      else
                        frames[i + 1][1]
                      end
-    # スペア
-    elsif frame.sum == 10
-      total_point += frames[i + 1][0]
     end
+  elsif frame.sum == 10
+    total_point += frames[i + 1][0]
   end
 end
 
